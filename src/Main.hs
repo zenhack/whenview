e9ee37140795d04000ec129c3b819f3a73de8334
Ian Denhardt <ian@zenhack.net>
2016-02-16 18:37:48 -0500

    Output *just* html
    The browser can actually render this. There are things that still need
    to be addressed though.

--- src/Main.hs
+++ src/Main.hs
@@ -3,7 +3,7 @@ import WhenView.Stage1.Serializer (calTokens)
 import Text.ParserCombinators.Parsec (runParser)
 import WhenView.Stage2.Parser (months)
 import WhenView.Html as H
-import Text.Blaze.Renderer.String (renderHtml)
+import Text.Blaze.Renderer.Pretty (renderHtml)
 import Control.Monad (mapM_)
 
 main = do
@@ -11,5 +11,7 @@ main = do
     let ents = runParser entries () "stdin" contents
     let toks = fmap calTokens ents
     let ast = fmap (runParser months () "tokens") toks
-    let markup = fmap (fmap $ fmap $ renderHtml . H.fromMonth) ast
-    print markup
+    case ast of
+        Right (Right result) -> mapM_ (putStrLn . renderHtml) (map H.fromMonth result)
+        Right (Left err) -> print err
+        Left err -> print err
