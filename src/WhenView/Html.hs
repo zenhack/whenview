3557b7c58c713d612ff66ea5d8808742725efbc7
Ian Denhardt <ian@zenhack.net>
2016-02-16 17:51:59 -0500

    Fix compilation errors.
    Pretty trivial.

--- src/WhenView/Html.hs
+++ src/WhenView/Html.hs
@@ -1,13 +1,13 @@
 module WhenView.Html where
 
-import Control.Monad (forM, mapM)
+import Control.Monad (forM_, mapM_)
 import qualified Text.Blaze.Html5 as H
 import WhenView.Data
 
 fromDay :: Day -> H.Html
 fromDay (Day _ items) = H.td $ do
-    H.ul $ forM items (\item ->
+    H.ul $ forM_ items (\item ->
         H.li (H.toHtml $ show item))
 
 fromWeek :: Week -> H.Html
-fromWeek (Week days) = H.tr $ mapM fromDay days
+fromWeek (Week days) = H.tr $ mapM_ fromDay days
