b7989f2863f1c29d967b3d804d8febdb3c9178ac
Ian Denhardt <ian@zenhack.net>
2016-02-16 20:40:08 -0500

    Display times in a friendlier format.
    The output of show isn't super presentable or concise. This is pretty
    awful too though.

--- src/WhenView/Html.hs
+++ src/WhenView/Html.hs
@@ -4,13 +4,23 @@ import Control.Monad (forM_, mapM_)
 import Data.Map.Strict as M
 import qualified Text.Blaze.Html5 as H
 import qualified Data.Hourglass as Hourglass
-import Data.Hourglass(WeekDay(..))
+import Data.Hourglass(WeekDay(..), TimeOfDay(..), Hours(..), Minutes(..))
 import WhenView.Data
+
+-- Display a time of day as a string like 18:04. XXX: there must be a way to do
+-- this in the hourglass package, but I'm not finding it at the moment.
+-- This is also sloppy as heck; it'll display 18:04 as 18:4 for example.
+formatTimeOfDay :: TimeOfDay -> String
+formatTimeOfDay tod = let (Hours h, Minutes m) = (todHour tod, todMin tod) in
+    (show h) ++ ":" ++ (show m)
+
+fromItem :: (Maybe TimeOfDay, String) -> H.Html
+fromItem (Nothing, s) = H.toHtml s
+fromItem (Just t, s) = H.toHtml (formatTimeOfDay t ++ ": " ++ s)
 
 fromDay :: Day -> H.Html
 fromDay (Day _ items) = H.td $
-    H.ul $ forM_ items (\item ->
-        H.li (H.toHtml $ show item))
+    H.ul $ forM_ items (\item -> H.li $ fromItem item)
 
 fromWeek :: Week -> H.Html
 fromWeek (Week days) = H.tr $ mapM_ fromDay days
