917b5a641c4ab23e8ed64fb61afeeb335be46d7e
Ian Denhardt <ian@zenhack.net>
2016-02-18 16:16:24 -0500

    Proper command line parsing, run when by ourselves.
    The CLI has an option --browser which it doesn't actually act upon yet.
    That will show up soon.

--- README.md
+++ README.md
@@ -5,22 +5,29 @@ to use it to see an actually *calendar* though. WhenView parses the output of
 `when`, and transforms it into an html document, which can then be passed to a
 browser for rendering.
 
-Example:
+By default, WhenView invokes `when` itself:
+
+    $ whenview > out.html
+    $ firefox out.html
+
+
+If WhenViews's first and only argument is `--stdin`, the input will be
+read from stdin:
 
     when --noheader | whenview > out.html
     firefox out.html
 
+Note that the `--noheader` option is required.
+
 Lynx can render from stdin:
 
-    when --noheader | whenview | lynx -dump -stdin
+    whenview | lynx -dump -stdin
 
 ...But unfortunately, lynx doesn't display the tables in a way that
 looks much like a calendar. If you want to use a command line browser,
 you can use Links:
 
-    when --noheader | whenview > out.html
+    whenview > out.html
     links out.html
-
-Note that the `--noheader` option is required.
 
 [1]: http://www.lightandmatter.com/when/when.html
--- src/Main.hs
+++ src/Main.hs
@@ -4,9 +4,36 @@ import Text.ParserCombinators.Parsec (runParser)
 import WhenView.Stage2.Parser (years)
 import WhenView.Html as H
 import Text.Blaze.Html.Renderer.Pretty (renderHtml)
+import System.Process (readProcess)
+import System.Environment (getArgs)
+import Options.Applicative hiding (runParser)
+import qualified Options.Applicative as A
+import Options.Applicative.Types (ArgPolicy(AllowOpts))
+import Control.Monad.Trans (liftIO)
+
+data Args = Args { stdin    :: Bool
+                 , browser  :: String
+                 , whenArgs :: [String]
+                 }
+
+argParser :: A.Parser Args
+argParser = Args
+    <$> switch (long "stdin" <> help "Read from stdin")
+    <*> strOption
+        ( long "browser"
+       <> short 'b'
+       <> value "xdg-open"
+       <> metavar "BROWSER"
+       <> help "The browser to invoke"
+        )
+    <*> (many $ argument str (metavar "WHEN_ARGS..."))
 
 main = do
-    contents <- getContents
+    argSpec <- execParser $ info argParser (progDesc "Render when calendars as html")
+    contents <- if stdin argSpec then
+            getContents
+        else
+            readProcess "when" ("--noheader":whenArgs argSpec) ""
     let ents = runParser entries () "stdin" contents
     let toks = fmap calTokens ents
     let ast = fmap (runParser years () "tokens") toks
